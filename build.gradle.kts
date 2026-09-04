3aa9b86e87ebb44ec4fe4bf0ab12cecd1e7ea02d
satkhiravoice9-collab <satkhiravoice9@gmail.com>
2026-09-04 18:29:24 +0600

    Add Google Services plugin for Firebase
    Added Google Services plugin for Firebase integration.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
     id("com.android.application") version "8.6.1" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    
+    // ফায়ারবেস গুগল সার্ভিসের জন্য নিচের লাইনটি যুক্ত করুন
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
